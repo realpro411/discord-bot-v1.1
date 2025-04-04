01bf567c1de4e5228d9e7291d88983fbe9a70972
realpro <alan.woodman11@gmail.com>
2025-04-04 19:39:41 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$merhaba'):
+        await message.channel.send("Selam!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('$rickroll'):
+        await message.channel.send("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+client.run("im not leaking my bot token lol")
