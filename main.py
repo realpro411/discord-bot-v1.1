b6c6223e82618de21a77e48aa5c42074bba4b4f8
realpro <alan.woodman11@gmail.com>
2025-04-18 19:37:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import discord
 from discord.ext import commands
+import os
+import random
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='--', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -12,18 +14,23 @@ async def on_ready():
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir tenekeyim!')
 
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
 @bot.command()
-async def troll(ctx, count_troll = 5):
-    await ctx.send(("⠀⠀⠀⠀⠀⠀⢀⣴⠶⠶⠶⠶⠶⢶⢶⡶⡶⢶⣶⣶⡶⡶⡶⣶⣶⣶⣤⣤⣀⣀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⣴⠏⠁⠀⣀⡰⣒⡭⣽⣒⣒⣒⣒⡒⠲⠲⠮⠯⠭⠷⠽⠽⠽⠭⠛⠷⣦⣀⠀⠀⠀\n⠀⠀⠀⠀⣼⠏⠀⠰⠚⡱⡪⠗⠉⠉⠀⠀⠀⠈⢇⠀⠀⠀⠀⢪⠋⠉⠉⠙⠲⡄⠀⠈⢹⡇⠀⠀\n⠀⠀⣠⣾⢇⠀⠀⠀⡈⡈⣡⣶⣾⣿⣿⡟⠷⣶⣄⠀⠀⠀⠀⠂⣠⣤⣴⣤⣄⢁⠀⠀⠘⣷⣄⠀\n⢠⣾⡟⢙⣹⣽⢵⣬⣝⢂⠛⠛⢙⣽⠏⠛⠷⣮⠿⠃⠀⠐⢶⣿⠿⠛⠛⠛⠙⠉⡙⡙⡽⡪⣟⣧\n⡟⢱⢡⣾⠃⢀⣶⣌⡙⠙⠛⠛⠛⠁⠀⠀⡀⢀⢀⠀⠀⠀⠈⣿⣄⠀⠀⢶⡴⡾⢛⠙⠻⢸⢸⣿\n⣷⣸⡘⣧⡚⠻⣯⡙⠛⢷⣦⣦⣄⡒⡒⠋⢽⡟⣭⣤⣄⠀⠀⠀⣹⣿⡦⣄⠀⢀⣾⣇⢀⣪⣼⡿\n⠘⢧⣕⡮⠅⠀⠹⣿⣷⣤⣿⣌⠉⠛⠻⢶⣮⣷⣬⣀⣀⣐⠳⢞⠋⣀⣀⣤⣵⣿⢿⣿⠈⢠⡿⠁\n⠀⠈⠻⣧⡀⠀⠀⠘⢷⡌⣿⠿⢿⣷⣦⣾⣇⣀⠈⠙⣿⠛⠛⣻⡟⠛⢙⣿⣀⣿⣼⣿⠇⢸⡇⠀\n⠀⠀⠀⠙⣧⡄⠀⠀⠀⠙⢿⣄⡀⠀⢹⡟⠛⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⢺⡇⠀\n⠀⠀⠀⠀⠘⢻⣦⡀⣄⡠⣀⡙⠻⢶⣾⣃⡀⠀⠀⢸⡇⠉⢉⣿⠋⢹⡟⢹⣟⣹⣿⠏⠀⢸⡇⠀\n⠀⠀⠀⠀⠀⠀⠈⠻⢶⣭⣚⠮⣑⡦⢅⣍⠛⠛⠟⠟⠗⠷⠿⠿⠞⠟⠟⠟⠛⢋⡄⠀⠀⢸⡇⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠿⢶⣭⣕⡳⠫⠾⢶⣶⣲⡨⠬⠬⠬⠤⠤⠴⠒⠋⣀⡔⠇⢸⡇⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⢶⣦⣄⣄⣁⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁⠀⢀⣼⠇⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠻⠳⠶⠶⣦⣤⣤⣤⣤⠶⠶⠛⠁⠀⠀") * count_troll)
-
-@bot.command()
 async def add(ctx, left: int, right: int):
     await ctx.send(left + right)
+
+@bot.command()
+async def mem(ctx):
+    resim = random.choice(os.listdir('memes'))
+    with open(f'memes/{resim}', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
 
 bot.run("token girin")
